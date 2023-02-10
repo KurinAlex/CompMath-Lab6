b961633807c383f20d22115705d17f1e20b67c9b
Kurin, Alex <olexkurin@gmail.com>
2023-02-10 23:14:56 +0200

    init

---
+++ CompMath-Lab6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CompMath_Lab6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
